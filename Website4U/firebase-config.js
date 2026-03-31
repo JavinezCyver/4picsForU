a55cf6391e9cbac05430c851ba832b353106db19
aeshirop <Aeshiro14@gmail.com>
2026-03-31 14:54:45 +0800

    Delete Website4U directory

--- Website4U/firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAeEjSGGtpvinwAVu8mlnTi5zZI-6Kljc0",
-  authDomain: "pics4u-84c87.firebaseapp.com",
-  projectId: "pics4u-84c87",
-  storageBucket: "pics4u-84c87.firebasestorage.app",
-  messagingSenderId: "587622230036",
-  appId: "1:587622230036:web:365583fbc290cf3fdb1f01",
-  measurementId: "G-EZG8B6Q9RY"
-};
